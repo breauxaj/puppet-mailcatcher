25a19571bebf7dd7154273468caa308163068af6
Adam Breaux <breauxaj@gmail.com>
2014-08-15 08:03:14 -0400

    added docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,33 @@
+# Class: mailcatcher
+#
+# This class installs Mailcatcher
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the mailcatcher package
+#
+# Actions:
+#   - Install Mailcactcher
+#
+# Requires:
+#
+#  Rubygems
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'mailcatcher':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'mailcatcher':
+#      ensure => 'absent'
+#    }
+#
 class mailcatcher (
   $ensure = 'latest'
 ){
